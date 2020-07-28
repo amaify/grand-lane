8bf00161dd0496cfe5656651a38ae75fc59f8527
amaify <john.ugwuanyi@yahoo.com>
2020-07-28 19:47:41 +0100

    Added Sitemaps

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+export default (
+  <Switch>
+    <Route path="/" />
+    <Route path="/about" />
+    <Route path="/service" />
+    <Route path="/reservation" />
+    <Route path="/contact" />
+    <Route />
+  </Switch>
+);
